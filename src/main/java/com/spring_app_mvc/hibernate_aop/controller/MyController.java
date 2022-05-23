97ed5f6dcf3ab87ec7cdc6877361871bc47aceb6
noleynik29 <noleynik29@gmail.com>
2022-05-23 13:24:05 +0300

    Java Spring App using: spring web mvc, javax servlet, mysql connector java, c3p0, spring orm, hibernate core and aspectj.

---
+++ src/main/java/com/spring_app_mvc/hibernate_aop/controller/MyController.java
@@ -0,0 +1,55 @@
+package com.spring_app_mvc.hibernate_aop.controller;
+
+import com.spring_app_mvc.hibernate_aop.entity.Employee;
+import com.spring_app_mvc.hibernate_aop.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class MyController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @RequestMapping("/")
+    public String showAllEmployees(Model model){
+
+        List<Employee> allEmployees = employeeService.getAllEmployees();
+        model.addAttribute("allEmps", allEmployees);
+        return "all-employees";
+    }
+
+    @RequestMapping("/addNewEmployee")
+    public String addNewEmployee(Model model){
+
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+
+        return "info-employee";
+    }
+
+    @RequestMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee){
+        employeeService.saveEmployee(employee);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/updateInfo")
+    public String updateEmployee(@RequestParam("empId") int id, Model model){
+        Employee employee = employeeService.getEmployee(id);
+        model.addAttribute("employee", employee);
+        return "info-employee";
+    }
+
+    @RequestMapping("/deleteEmployee")
+    public String deleteEmployee(@RequestParam("empId") int id){
+        employeeService.deleteEmployee(id);
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/com/spring_app_mvc/hibernate_aop/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package com.spring_app_mvc.hibernate_aop.service;
+
+import com.spring_app_mvc.hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+}
